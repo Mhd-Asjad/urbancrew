1d6caa53605bc3bcbe9c3cbafc0048842b5a39b4
mhd asjad <mhdasjad877@gmail.com>
2025-10-29 17:40:24 +0530

    Add whitenoise for static files

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: cd ecommerce && python manage.py migrate && python manage.py collectstatic --noinput
 web: cd ecommerce && gunicorn ecommerce.wsgi:application --bind 0.0.0.0:$PORT
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -66,6 +66,7 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",    
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
